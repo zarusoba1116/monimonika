51cd53d5609741eff63919d3b314bc613f10fb0f
zarusoba1116 <91121948+zarusoba1116@users.noreply.github.com>
2024-03-29 12:58:38 +0900

    asdasd

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
+from os import getenv
 
+TOKEN = getenv('DISCORD_BOT_TOKEN')
+intents = discord.Intents.all()
+intents.typing = False
+bot = commands.Bot(command_prefix='$',help_command=None,case_insensitive=True,intents=intents)
+
+@bot.listen("on_message")
+async def on_message(message):
+    message.channel.send("Just Monika.")
+
+bot.run(TOKEN)
