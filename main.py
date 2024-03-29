2360925807495ba382ab08d705543fbc0ba5da09
zarusoba1116 <91121948+zarusoba1116@users.noreply.github.com>
2024-03-29 13:34:54 +0900

    asdd

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ bot = commands.Bot(command_prefix='$',help_command=None,case_insensitive=True,in
 
 @bot.listen("on_message")
 async def on_message(message):
-<<<<<<< HEAD
     if message.author.bot:
         return
     await message.channel.send("Just Monika.")
-=======
-    await message.channel.send("Just Monika.")
->>>>>>> b4e4260bab493cd2a5338232370b2616264b7795
 
 bot.run(TOKEN)
