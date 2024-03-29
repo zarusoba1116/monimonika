dba007b43c086aa39bdb194dcd5cd218ea8af495
zarusoba <zarusoba327@gmail.com>
2024-03-29 14:49:18 +0900

    jhgvjh

--- main.py
+++ main.py
@@ -43,51 +43,51 @@ class ChinchirorinBot(discord.Client):
         else:
             await message.channel.send(f'{message.author.mention} は既にゲームに参加しています。')
 
-async def play_round(self, message):
-    if len(self.players) < 2:
-        await message.channel.send('参加者が足りません。')
-        return
+    async def play_round(self, message):
+        if len(self.players) < 2:
+            await message.channel.send('参加者が足りません。')
+            return
 
-    # 親の選択
-    parent_id = max(self.players, key=self.players.get)
-    parent_fund = self.players[parent_id]
+        # 親の選択
+        parent_id = max(self.players, key=self.players.get)
+        parent_fund = self.players[parent_id]
 
-    # ゲーム開始メッセージ
-    start_message = f'ゲームを開始します。親は {message.guild.get_member(parent_id).mention} です。'
-    await message.channel.send(start_message)
+        # ゲーム開始メッセージ
+        start_message = f'ゲームを開始します。親は {message.guild.get_member(parent_id).mention} です。'
+        await message.channel.send(start_message)
 
-    # ゲーム実行
-    for player_id, fund in self.players.items():
-        if player_id != parent_id:
-            player = message.guild.get_member(player_id)
-            await message.channel.send(f'{player.mention} の資金: {fund}')
+        # ゲーム実行
+        for player_id, fund in self.players.items():
+            if player_id != parent_id:
+                player = message.guild.get_member(player_id)
+                await message.channel.send(f'{player.mention} の資金: {fund}')
 
-            # サイコロを振る
-            dice_result = random.randint(1, 6)
-            await message.channel.send(f'{player.mention} がサイコロを振りました。結果: {dice_result}')
+                # サイコロを振る
+                dice_result = random.randint(1, 6)
+                await message.channel.send(f'{player.mention} がサイコロを振りました。結果: {dice_result}')
 
-            # ゲーム結果の判定と処理
-            if dice_result == 6:
-                await message.channel.send('親の勝ちです！コマの総取りです。')
-                parent_fund += fund
-                self.players[player_id] = 0
-            elif dice_result == 4 or dice_result == 5:
-                await message.channel.send('親の勝ちです！コマの倍額を受け取ります。')
-                parent_fund += fund
-                self.players[player_id] = 0
-            elif dice_result == 1 or dice_result == 2 or dice_result == 3:
-                await message.channel.send('親の負けです。コマを支払います。')
-                parent_fund -= fund
-                self.players[player_id] = 0
-            await message.channel.send(f'{message.guild.get_member(parent_id).mention} の残り資金: {parent_fund}')
+                # ゲーム結果の判定と処理
+                if dice_result == 6:
+                    await message.channel.send('親の勝ちです！コマの総取りです。')
+                    parent_fund += fund
+                    self.players[player_id] = 0
+                elif dice_result == 4 or dice_result == 5:
+                    await message.channel.send('親の勝ちです！コマの倍額を受け取ります。')
+                    parent_fund += fund
+                    self.players[player_id] = 0
+                elif dice_result == 1 or dice_result == 2 or dice_result == 3:
+                    await message.channel.send('親の負けです。コマを支払います。')
+                    parent_fund -= fund
+                    self.players[player_id] = 0
+                await message.channel.send(f'{message.guild.get_member(parent_id).mention} の残り資金: {parent_fund}')
 
-    # 新しい親の選択
-    new_parent_id = max(self.players, key=self.players.get)
-    if new_parent_id != parent_id:
-        await message.channel.send(f'{message.guild.get_member(new_parent_id).mention} が新しい親になりました。')
-    else:
-        await message.channel.send('ゲーム終了です。')
-    self.players = {new_parent_id: parent_fund}  # 新しい親のみ残す
+        # 新しい親の選択
+        new_parent_id = max(self.players, key=self.players.get)
+        if new_parent_id != parent_id:
+            await message.channel.send(f'{message.guild.get_member(new_parent_id).mention} が新しい親になりました。')
+        else:
+            await message.channel.send('ゲーム終了です。')
+        self.players = {new_parent_id: parent_fund}  # 新しい親のみ残す
 
 
 bot.run(TOKEN)
